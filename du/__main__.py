f4a4f021847ebb7588fc228d874f74848d4265d8
Nikola Spiric <nikola.spiric@rt-rk.com>
2016-10-01 08:09:24 +0200

    Add artifact app

--- du/__main__.py
+++ du/__main__.py
@@ -4,30 +4,28 @@ import sys
 from du.android.smartpush.AndroidSmartPushApp import main as smartpushMain
 from du.drepo.App import main as drepoMain
 from du.drepo.indexer.App import main as indexerMain
-
+from du.android.smartpush.AndroidArtifactTool import main as artifactMain
 
 def main():
     apps = {
         'drepo' : drepoMain,
         'sp' : smartpushMain,
-        'drepo-index' : indexerMain
+        'drepo-index' : indexerMain,
+        'artifact' : artifactMain
     }
 
+    if len(sys.argv) < 2:
+        print('Available apps: ' + str(apps.keys()))
+        return -1
 
+    appName = sys.argv[1]
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('appName',
-                        help='Available apps: ' + str(apps.keys()))
-
-    args = parser.parse_args()
-
-    if args.appName not in apps:
-        print('Invalid app name: %r' % args.appName)
+    if appName not in apps:
+        print('Invalid app name: %r' % appName)
         return -1
     else:
         sys.argv.pop(1)
-        return apps[args.appName]()
+        return apps[appName]()
 
 if __name__ == '__main__':
     sys.exit(main())
