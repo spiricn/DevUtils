baeb5940942d7d076707863c9118ec6f1dbe1a47
Nikola Spiric <nikola.spiric@rt-rk.com>
2017-06-09 10:34:02 +0200

    [ctee] Right align logcat tag

--- du/ctee/processors/LogcatProcessor.py
+++ du/ctee/processors/LogcatProcessor.py
@@ -1,5 +1,3 @@
-import time
-
 from du.android import LogcatParser
 from du.android.LogcatParser import INVERSE_LOGCAT_LEVEMAP
 from du.ctee.processors.BaseProcessor import BaseProcessor
@@ -8,6 +6,7 @@ from du.ctee.processors.BaseProcessor import BaseProcessor
 class LogcatProcessor(BaseProcessor):
     def __init__(self, stylesheet=None):
         BaseProcessor.__init__(self, stylesheet)
+        self._maxTagLen = 0
 
     @staticmethod
     def getDefaultStylesheet():
@@ -33,8 +32,11 @@ class LogcatProcessor(BaseProcessor):
 
         paddingSize = 5 - len(str(parsed.pid))
 
+        self._maxTagLen = max(self._maxTagLen, len(parsed.tag))
+        fmt = '{:>' + str(self._maxTagLen) + '}'
+
         paddedPid = (paddingSize * '0') + str(parsed.pid)
-        meta += ' ' + INVERSE_LOGCAT_LEVEMAP[parsed.level] + ' ' + paddedPid + ' ' + parsed.tag + ':'
+        meta += ' ' + INVERSE_LOGCAT_LEVEMAP[parsed.level] + ' ' + paddedPid + ' ' + fmt.format(parsed.tag) + ':'
 
         return (
             (meta, self.stylesheet['meta']), (' ', None), (parsed.message, self._findStyle(line))
